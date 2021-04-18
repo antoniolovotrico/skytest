3665dd0ebc5dd545b8fcef9a5d209b3a554dd413
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-04-18 20:55:24 +0200

    task 4 completed

--- task4/assets/js/main.js
+++ task4/assets/js/main.js
@@ -3,44 +3,44 @@ var shoes = [
   {
     name: "Smith",
     price: 180,
-    size: [39, 40, 41],
+    size: [38, 39, 40, 41, 42, 43, 44, 45],
     desc: "Sneakers della linea Portofino",
     patch: [
       {
         inner: [
           {
-            name: "wmelon",
+            name: "Watermelon",
             pic: "assets/img/patch/AACCS202387881-1.png",
             price: 50,
           },
           {
-            name: "straw",
+            name: "Strawberries",
             pic: "assets/img/patch/AACCS00204380311-1.png",
-            price: 50,
+            price: 60,
           },
           {
-            name: "heart",
+            name: "Heart",
             pic: "assets/img/patch/ADECO02796B80303-1.png",
-            price: 50,
+            price: 70,
           },
         ],
       },
       {
         outer: [
           {
-            name: "wmelon",
+            name: "Watermelon",
             pic: "assets/img/patch/AACCS202387881.png",
             price: 50,
           },
           {
-            name: "straw",
+            name: "Strawberries",
             pic: "assets/img/patch/AACCS00204380311.png",
-            price: 50,
+            price: 60,
           },
           {
-            name: "heart",
+            name: "Heart",
             pic: "assets/img/patch/ADECO02796B80303.png",
-            price: 50,
+            price: 70,
           },
         ],
       },
@@ -53,17 +53,8 @@ var shoes = [
             pics: "assets/img/scarpa-sinistra/Sneakers_left_shoe_front.png",
           },
           {
-            side: "inner",
-            pics: "assets/img/scarpa-sinistra/left_inner_8051124195013_6.png",
-          },
-          {
-            side: "outer",
-            pics: "assets/img/scarpa-sinistra/left_outer_8051124195013_5.png",
-          },
-
-          {
             side: "back",
-            pics: "assets/img/scarpa-sinistra/left_back_8051124195013_7.png",
+            pics: "assets/img/scarpa-destra/right_back_8051124195013_3.png",
           },
         ],
         right: [
@@ -72,22 +63,16 @@ var shoes = [
             pics: "assets/img/scarpa-destra/Sneakers_right_shoe_front.png",
           },
           {
-            side: "inner",
-            pics: "assets/img/scarpa-destra/right_inner_8051124195013_1.png",
-          },
-          {
-            side: "outer",
-            pics: "assets/img/scarpa-destra/right_outer_8051124195013_2.png",
-          },
-          {
             side: "back",
-            pics: "assets/img/scarpa-destra/right_back_8051124195013_3.png",
+            pics: "assets/img/scarpa-sinistra/left_back_8051124195013_7.png",
           },
         ],
       },
     ],
   },
 ];
+
+var carrello = {};
 
 const menuHeader = document.querySelector("header ul");
 const menuFooter = document.querySelector("footer ul");
@@ -100,6 +85,8 @@ const cartPop = document.querySelector(".pop_hide");
 const modalBtn = document.querySelector("#modal_btn");
 const patchInner = document.querySelector(".patch_inner");
 const patchOuter = document.querySelector(".patch_outer");
+const attachContInn = document.querySelector(".attach_container_1");
+const attachContOut = document.querySelector(".attach_container_2");
 
 for (let link of linkMenu) {
   menuHeader.innerHTML += `<li><a href="#">${link}</a></li>`;
@@ -107,11 +94,15 @@ for (let link of linkMenu) {
 }
 
 for (let shoe of shoes) {
-  shoesDiv.innerHTML += `<h2>${shoe.name}</h2>
+  shoesDiv.innerHTML += `<h1>${shoe.name}</h1>
     <h3>${shoe.price} €</h3>
     <h3>${shoe.desc}</h3>`;
   selectSize.innerHTML = `
     <option disabled selected value> - select a size - </option>`;
+    
+    var selectedName = { shoeName: shoe.name, shoePrice: shoe.price};
+    carrello = {...carrello, ...selectedName};
+    
 
   for (let size of shoe.size) {
     selectSize.innerHTML += `
@@ -122,38 +113,25 @@ for (let shoe of shoes) {
 
   for (let side of shoe.image) {
     for (let right of side.right) {
-      if (right.side == "front") {
         shoesImgRight.innerHTML += `
-        <img class="img_front'" src="${right.pics}">`;
-      } else {
-        shoesImgRight.innerHTML += `<div class="popi">
-        <img class="shoe_img" src="${right.pics}">
-        </div>`;
-      }
+        <img class="img_front" src="${right.pics}">`;
     }
 
-    for (let left of side.left) {
-      if (left.side == "front") {
+    for (let left of side.left) { 
         shoesImgLeft.innerHTML += `
-        <img class="img_front'" src="${left.pics}">`;
-      } else {
-        shoesImgLeft.innerHTML += `<div class="popi">
-        <img class="shoe_img" src="${left.pics}">
-        </div>`;
-      }
+        <img class="img_front" src="${left.pics}">`;
     }
   }
   
   for (let patch of shoe.patch) {
     if (patch.inner) {
       patch.inner.forEach((element,i) => {
-        // console.log(element);
-        patchInner.innerHTML += `<img class="patch_img" onclick="attPatch(${i})" src="${element.pic}">`;
+        patchInner.innerHTML += `<img class="patch_img" onclick="attPatchOuter(${i})" src="${element.pic}">`;
        
       });
     } else if (patch.outer) {
       patch.outer.forEach((element,i) => {
-        patchOuter.innerHTML += `<img class="patch_img" onclick="attPatch(${i})" src="${element.pic}">`;
+        patchOuter.innerHTML += `<img class="patch_img" onclick="attPatchInner(${i})" src="${element.pic}">`;
         
       });
     }
@@ -164,35 +142,51 @@ for (let shoe of shoes) {
 selectSize.addEventListener("change", (event) => {
   //creare una variabile che identifichi la scelta compiuta con event change
   var choice = selectSize.options[selectSize.selectedIndex].value;
-  shoes[0]["choice"] = choice;
+  carrello["choice"] = choice;
+  
 });
 
 cartBtn.addEventListener("click", (event) => {
-  addToCart(shoes, cartPop);
+  addToCart(carrello, cartPop);
 });
 
-function attPatch(i){
+function attPatchOuter(i){
   
   for (let shoe of shoes) {
     for (let patch of shoe.patch) {
       if (patch.inner) {
-        console.log(patch.inner[i]);
         var pinner = [patch.inner[i]];
         pinner.forEach((element) => {
           
-          shoesImgRight.innerHTML += `
-          <img class="app_img_hide" src="${element.pic}">`;
-        });
-      } else if (patch.outer) {
-        var pouter = [patch.outer[i]]
-        pouter.forEach((element) => {
-          // console.log(element);
-          shoesImgLeft.innerHTML.innerHTML = `
-          <img class="app_img_hide" src="${element.pic}">`;
+          attachContOut.innerHTML = `
+          <img class="attach" src="assets/img/scarpa-sinistra/left_outer_8051124195013_5.png" alt="">
+          <img class="app_img_out" src="${element.pic}">`;
+          var outerPatch = {outChooseName: element.name, outChooseImg: element.pic, outChoosePrice: element.price};
+          carrello = {...carrello, ...outerPatch};
         });
       }
     }
+  }
+};
+
+function attPatchInner(i){
+  
+  for (let shoe of shoes) {
+    for (let patch of shoe.patch) {
+     
+       if (patch.outer) {
+        var pouter = [patch.outer[i]]
+        pouter.forEach((element) => {
+          attachContInn.innerHTML = `
+          <img class="attach" src="assets/img/scarpa-sinistra/left_inner_8051124195013_6.png" alt="">
+          <img class="app_img_inn" src="${element.pic}">`;
+          var innerPatch = {innChooseName: element.name, innChooseImg: element.pic, innChoosePrice: element.price};
+          carrello = {...carrello, ...innerPatch};
+          console.log(carrello);
+        });
+      }
     }
+  }
 };
 
 /* ---- FUNCTIONS ----*/
@@ -201,17 +195,41 @@ function addToCart(model, popCart) {
   popCart.classList.remove("pop_hide");
   popCart.classList.add("pop_show");
 
-  model.forEach((element) => {
-    popCart.innerHTML = `
-    <h4>Model : ${element.name}</h4>
-    <h4>Price : ${element.price} €</h4>
-    <h4>Size :${element.choice}</h4>
-    <button id="modal_btn" onclick="closeModal(cartPop)">Close</button>
-    `;
-  });
+    popCart.innerHTML = ` 
+    <div class="title_cart">
+      <h2>Model : ${model.shoeName}</h2>
+      <h2>Price : ${model.shoePrice} €</h2>
+      <h2>Size :${model.choice}</h2>
+    </div>
+    <div class="img_cart">
+      <div class="out_cart">
+        <h4>${model.outChooseName}</h4>
+        <div class="attach_container_2">
+        <img class="attach" src="assets/img/scarpa-sinistra/left_outer_8051124195013_5.png" alt="">
+        <img class="app_img_out" src="${model.outChooseImg}">
+        </div>
+        <h4>Costo Patch ${model.outChoosePrice} €</h4>
+      </div>
+      <div class="inn_cart">
+        <h4>${model.innChooseName}</h4>
+        <div class="attach_container_1">
+        <img class="attach" src="assets/img/scarpa-sinistra/left_inner_8051124195013_6.png" alt="">
+        <img class="app_img_inn" src="${model.innChooseImg}">
+        </div>
+        <h4>Costo Patch ${model.innChoosePrice} €</h4>
+      </div>
+    </div>
+    <h2>Totale ${totalFunc(model.shoePrice,model.outChoosePrice,model.innChoosePrice)} €</h2>
+    <button id="modal_btn" onclick="closeModal(cartPop)">Close</button>`;
+    
 }
 
 function closeModal(popCart) {
   popCart.classList.remove("pop_show");
   popCart.classList.add("pop_hide");
+}
+
+function totalFunc(a,b,c){
+  var tot = "";
+  return tot = a + b + c;
 }
